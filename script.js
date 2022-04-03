fc1c988884049c89b7719a8e24e8d2d6966fc4d3
vano31 <vanosike31@gmail.com>
2022-04-03 01:18:21 -0400

    Add hover and click version for box color change

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const wind = document.querySelector(`#window`);
 
 let input = prompt(`Please type the amount of boxes you want to make up the height and width`);
 let numberBox = parseInt(input);
+let i;
 
 let boxCalculator = function() {
-
         let totalBox = numberBox ** 2;
         const boxContainer = [];
-        for (let i = 0; i < totalBox; i++) {
+        for (i = 0; i < totalBox; i++) {
             boxContainer[i] = document.createElement(`div`);
             boxContainer[i].classList.add(`box`);
             let boxSideLength =  ((700  / numberBox));
@@ -17,11 +17,34 @@ let boxCalculator = function() {
             wind.appendChild(boxContainer[i]);
         }
         return totalBox;
-    
 }
 
-
 boxCalculator();
+
+
+const boxes = document.querySelectorAll(`.box`); 
+
+boxes.forEach(box => {
+    box.addEventListener(`click`, function clickOutcome() {
+        box.classList.add(`filled`);
+    })
+
+    box.addEventListener(`mouseover`, function hoverOver() {
+        box.classList.add(`hovered`);
+    })
+
+    box.addEventListener(`mouseleave`, function hoverLeave() {
+        box.classList.remove(`hovered`);
+    })
+
+
+
+});
+
+
+
+
+
 //let totalBox = numberBox ** 2;
 
 
